16c692764b4b1da416f728930a6abff46b5b3d07
mukesh1327 <mugicoder13@gmail.com>
2025-08-04 10:13:20 +0530

    Added dotnet hello-world

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
